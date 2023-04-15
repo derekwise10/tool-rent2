f562781b064e7a56599e6e0aab83d82bfb7ebf6c
danieldietz6 <danieldietz6@gmail.com>
2023-04-15 12:05:47 -0400

    Added descriptions to Tool page

--- screens/ToolScreen.js
+++ screens/ToolScreen.js
@@ -1,56 +1,65 @@
 import React from "react"
-import {StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import {StyleSheet, Text, View, SafeAreaView, Image, ScrollView, TouchableOpacity } from "react-native";
 import tw from 'tailwind-react-native-classnames';
 
-const ToolScreen = () => {
-    return (
-   <SafeAreaView style={tw `bg-white h-full p-10`}>
-          <View>
-          <Text style={tw `mt-2 text-lg font-semibold`}>Select A Tool To rent</Text>
-          <Text style={tw `mt-2 text-lg font-semibold`}>Selecting A Tool displays details</Text>
-          <Image
-                 style={{
-                 width: 200,
-                 height:200,
-                  resizeMode: "contain",
-                 }}
-                 source ={{
-                 uri: "https://i.ebayimg.com/00/s/MTIyM1gxNjAw/z/H7EAAOSwIz5ceGU5/$_57.PNG?set_id=8800005007",
+const ToolScreen = ({navigation}) => {
+  const tools = [
+    {
+      name: "Power Drill",
+      renter: "John Smith",
+      price: "$20/day",
+      image: "https://i.ebayimg.com/00/s/MTIyM1gxNjAw/z/H7EAAOSwIz5ceGU5/$_57.PNG?set_id=8800005007"
+    },
+    {
+      name: "Circular Saw",
+      renter: "Jane Doe",
+      price: "$30/day",
+      image: "https://i.ebayimg.com/00/s/NTc2WDEwMjQ=/z/y3kAAOSwWxNYsWJR/$_86.JPG"
+    },
+    {
+      name: "Angle Grinder",
+      renter: "Bob Johnson",
+      price: "$25/day",
+      image: "https://jamaicaclassifiedonline.com/images/2020/10/28/134621/power-tools-xj7q3sly_0.jpg"
+    }
+  ];
 
-                 }}
+  const handleToolPress = (tool) => {
+    // Navigate to the details screen and pass the tool data as a parameter
+    navigation.navigate("ToolDetails", { tool });
+  }
 
-                 />
-                 <Image
-                                  style={{
-                                  width: 200,
-                                  height:200,
-                                   resizeMode: "contain",
-                                  }}
-                                  source ={{
-                                  uri: "https://i.ebayimg.com/00/s/NTc2WDEwMjQ=/z/y3kAAOSwWxNYsWJR/$_86.JPG",
-
-                                  }}
-
-                                  />
-                                       <Image
-                                                                    style={{
-                                                                    width: 200,
-                                                                    height:200,
-                                                                     resizeMode: "contain",
-                                                                    }}
-                                                                    source ={{
-                                                                    uri: "https://jamaicaclassifiedonline.com/images/2020/10/28/134621/power-tools-xj7q3sly_0.jpg",
-
-                                                                    }}
-
-                                                                    />
-    </View>
+  return (
+    <SafeAreaView style={tw `bg-white h-full p-10`}>
+      <ScrollView>
+        <View>
+          <Text style={tw `mt-2 text-lg font-semibold`}>Select A Tool To Rent</Text>
+          <Text style={tw `mt-2 text-lg font-semibold`}>Selecting A Tool Displays Details</Text>
+          {tools.map((tool, index) => (
+            <TouchableOpacity key={index} onPress={() => handleToolPress(tool)}>
+              <View>
+                <Image
+                  style={{
+                    width: 200,
+                    height: 200,
+                    resizeMode: "contain",
+                  }}
+                  source={{
+                    uri: tool.image,
+                  }}
+                />
+                <Text style={tw `mt-2 text-lg font-semibold`}>{tool.name}</Text>
+                <Text style={tw `mt-2`}>Renter: {tool.renter}</Text>
+                <Text style={tw `mt-2`}>Price: {tool.price}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
+  );
+};
 
-    )
+export default ToolScreen;
 
-}
-
-export default ToolScreen
-
-const style = StyleSheet.create({})
+const style = StyleSheet.create({});
